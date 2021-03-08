5699947b67e5aae663f101c96eb5f2035d355047
czM1K3 <misamadera1@gmail.com>
2021-03-08 15:12:13 +0100

    Changed Czech to English

--- Vase/MainWindow.xaml.cs
+++ Vase/MainWindow.xaml.cs
@@ -20,83 +20,83 @@ namespace Vase
     /// </summary>
     public partial class MainWindow : Window
     {
-        int alf = 30, bet = 30, uhel = 10;
-        double zoom = 0.5, velikost = 10;
+        int alf = 30, bet = 30, angle = 10;
+        double zoom = 0.5, size = 10;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        private void gridSelect_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (sender is Grid selector)
             {
-                Point mys = e.GetPosition(selector);
-                Ellipse kolecko = new Ellipse
+                Point mouse = e.GetPosition(selector);
+                selector.Children.Add(new Ellipse
                 {
-                    Height = velikost,
-                    Width = velikost,
-                    Margin = new Thickness(mys.X - velikost / 2, mys.Y - velikost / 2, 0 ,0),
+                    Height = size,
+                    Width = size,
+                    Margin = new Thickness(mouse.X - size / 2, mouse.Y - size / 2, 0, 0),
                     HorizontalAlignment = HorizontalAlignment.Left,
                     VerticalAlignment = VerticalAlignment.Top,
                     Fill = Brushes.Red,
-                };
-                selector.Children.Add(kolecko);
+                });
             }
         }
 
-        private void btnVykreslit_Click(object sender, RoutedEventArgs e)
+        private void btnRender_Click(object sender, RoutedEventArgs e)
         {
-            Vykresli();
+            Draw();
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
             gridSelect.Children.Clear();
+            Draw();
         }
 
-        void Vykresli()
+        void Draw()
         {
             gridik.Children.Clear();
-            Line cara = new Line();
+            Line line = new Line();
 
-            List<Point> body = gridSelect.Children.Cast<Ellipse>().Select(x => new Point(x.Margin.Left + velikost / 2, 150 - x.Margin.Top + velikost / 2)).OrderBy(x => x.Y).ToList();
+            List<Point> body = gridSelect.Children.Cast<Ellipse>().Select(x => new Point(x.Margin.Left + size / 2, 150 - x.Margin.Top + size / 2)).OrderBy(x => x.Y).ToList();
             if (body.Count > 0)
             {
-                for (int j = 0; j < 360; j += uhel)
+                for (int j = 0; j < 360; j += angle)
                 {
-                    Point bod1 = Axon3Dto2D(alf, bet, body[0].X, 0, body[0].Y, zoom, "z", j);
+                    Point point1 = Axon3Dto2D(alf, bet, body[0].X, 0, body[0].Y, zoom, "z", j);
                     for (int i = 1; i < body.Count; i++)
                     {
-                        Point bod2 = Axon3Dto2D(alf, bet, body[i].X, 0, body[i].Y, zoom, "z", j);
-                        cara = new Line
+                        Point point2 = Axon3Dto2D(alf, bet, body[i].X, 0, body[i].Y, zoom, "z", j);
+                        line = new Line
                         {
                             Stroke = Brushes.Red,
-                            X1 = bod1.X,
-                            Y1 = bod1.Y,
-                            X2 = bod2.X,
-                            Y2 = bod2.Y
+                            X1 = point1.X,
+                            Y1 = point1.Y,
+                            X2 = point2.X,
+                            Y2 = point2.Y
                         };
-                        gridik.Children.Add(cara);
-                        bod1 = bod2;
+                        gridik.Children.Add(line);
+                        point1 = point2;
                     }
 
                     for (int i = 0; i < body.Count; i++)
                     {
-                        Point bod3 = Axon3Dto2D(alf, bet, body[i].X, 0, body[i].Y, zoom, "z", j);
-                        double X = body[i].X * Math.Cos(uhel * Math.PI / 180) - (0 * Math.Sin(uhel * Math.PI / 180));
-                        double Y = body[i].X * Math.Sin(uhel * Math.PI / 180) + (0 * Math.Cos(uhel * Math.PI / 180));
-                        Point bod4 = Axon3Dto2D(alf, bet, X, Y, body[i].Y, zoom, "z", j);
-                        cara = new Line
+                        Point point3 = Axon3Dto2D(alf, bet, body[i].X, 0, body[i].Y, zoom, "z", j);
+                        double X = body[i].X * Math.Cos(angle * Math.PI / 180) - (0 * Math.Sin(angle * Math.PI / 180));
+                        double Y = body[i].X * Math.Sin(angle * Math.PI / 180) + (0 * Math.Cos(angle * Math.PI / 180));
+                        Point point4 = Axon3Dto2D(alf, bet, X, Y, body[i].Y, zoom, "z", j);
+                        line = new Line
                         {
                             Stroke = Brushes.Red,
-                            X1 = bod3.X,
-                            Y1 = bod3.Y,
-                            X2 = bod4.X,
-                            Y2 = bod4.Y
+                            X1 = point3.X,
+                            Y1 = point3.Y,
+                            X2 = point4.X,
+                            Y2 = point4.Y
                         };
-                        gridik.Children.Add(cara);
+                        gridik.Children.Add(line);
                     }
                 }
             }
